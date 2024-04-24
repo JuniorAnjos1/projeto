247e3b4b81e139b4d704a6f692fe5cc942c459cd
Junior <juninho7506@gmail.com>
2024-04-24 11:36:29 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // PEGAR A TAG IMG
+  const img = document.querySelector("#profile img")
+  // SUBSTITUIR A IMAGEM
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assents/avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assents/avataar.png")
+  }
+
+  // SUBSTITUIR A IMAGEM
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
+}
